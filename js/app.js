51b1d60554491aa36f805c545efdc2e98b2a6932
Buki <Bukithompson@gmail.com>
2017-05-28 12:26:14 +0100

    mvp down

--- js/app.js
+++ js/app.js
@@ -16,7 +16,6 @@ $(() => {
     });
   });
 
-
   ////////SHUFFLE DECK////
 
   function shuffle(array){
@@ -34,24 +33,6 @@ $(() => {
   shuffle(deck);
   console.log('reshuffled deck',deck);
 
-  //////////dealing decks
-  //function deal(){
-    // const hands = [];
-    // const players = $('#numOfplayers').val();
-    // const numOfCards = $('#numOfCards').val();
-    // console.log(players);
-    // console.log(numOfCards);
-    //
-    // for ( let i = 0; i < players; ++i) {
-    //   const playerHand = [];
-    //   for (let i = 0; i < numOfCards; i++) {
-    //     const card = deck.shift();
-    //     playerHand.push(card);
-    //   }
-    //   hands.push(playerHand);
-    //   console.log(playerHand);
-    // }
-  //}
   const hands = [];
   $('.submit').on('click', deal);
 
@@ -67,18 +48,15 @@ $(() => {
         for (let i = 0; i < numOfCards; i++) {
           const card = deck.shift();
           playerHand.push(card);
-          const eachPlayer = JSON.stringify(playerHand);
-          $demo.append(eachPlayer);
-          console.log('this is the players hands',playerHand);
-          console.log('these are the eachplayer', eachPlayer);
-
+          // const eachPlayer = JSON.stringify(playerHand);
+          // console.log('this is the playerHands array',playerHand);
+          // //console.log('these are the hands of each player stringified (eachplayer)', eachPlayer);
+          // $demo.append(eachPlayer, '<br/>');
         }
-        console.log('these are the hands');
         hands.push(playerHand);
 
-
       }
-      // $demo.append(hands);
+      displayHands();
       findWinner();
       console.log( 'these are the hands inide the deal function', hands);
     } else {
@@ -88,25 +66,30 @@ $(() => {
       console.log('these are the number of players',players);
       console.log('these are the number of cards',numOfCards);
     }
-
   }
-  // var obj = { "name":"John", "age":30, "city":"New York"};
-  // var myJSON = JSON.stringify(obj);
-  // document.getElementById("demo").innerHTML = myJSON;
-////FIND WINNER
 
+  function displayHands() {
+    for (let i = 0; i < hands; i++) {
+      const eachPlayer = JSON.stringify(playerHand);
+      console.log('this is the playerHands array',playerHand);
+      //console.log('these are the hands of each player stringified (eachplayer)', eachPlayer);
+      $demo.append(eachPlayer, '<br/>');
+    }
+  }
+
+/////////////////function to find th winner //////////////////
   function findWinner(){
     console.log('these are the hands inside the winner function', hands);
     const totals = hands.map((hand) => {
       return hand.reduce((total, card) => {
         return total + card.value;
-      });
+      }, 0);
     });
     console.log('this is the total within the findWinner function', totals);
-    const winningValue = Math.max(totals);
+    const winningValue = totals.reduce((a, b) => Math.max(a,b));
     const winningIndex = totals.indexOf(winningValue);
     console.log('the winning number', winningValue);
-    const winner = `Player ${winningIndex} won`;
+    const winner = `Player ${winningIndex + 1} won`;
     console.log(winner);
   }
 
